36b2fde81a940c7dde16cb3b699dd0d93b7747ab
erwin-willems <github-ew@reflexlight.nl>
2025-08-12 22:06:22 +0200

    code cleanup

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -8,13 +8,7 @@ import (
 
 func ValidatePayload(publicKey, payload, signature string) (bool, error) {
 	// Decode the public key from hex
-	pubKeyBytes, err := hex.DecodeString(publicKey)
-	if err != nil {
-		return false, err
-	}
-
-	// Decode the payload from hex
-	payloadBytes, err := hex.DecodeString(payload)
+	publicKeyBytes, err := hex.DecodeString(publicKey)
 	if err != nil {
 		return false, err
 	}
@@ -26,25 +20,10 @@ func ValidatePayload(publicKey, payload, signature string) (bool, error) {
 	}
 
 	// Hash the payload using SHA-256
-	hash := sha256.Sum256(payloadBytes)
+	hash := sha256.Sum256([]byte(payload))
 
 	// Verify the signature using secp256k1
-	valid := crypto.VerifySignature(pubKeyBytes, hash[:], signatureBytes)
+	valid := crypto.VerifySignature(publicKeyBytes, hash[:], signatureBytes)
 
 	return valid, nil
 }
-
-// func main() {
-// 	payload := `{"command":"curl ifconfig.me"}`
-// 	signature := "dad5578ac66504e1e8173cda2edef04cebd68e1ef854f9bfec11f5dfe0f55e0510ebf5c4c4ce5dd51afc9f5964135d56ea32ab29554b225d7a45c024dfe25f61"
-// 	publicKey := "049634ad321cac90c3f35a5f6563a0d71114c0e5728a3c47fe296a2e61071ed4385b8d8f19770deebebc8aa423242d7137f87986b32bff00c8d1b7c2b551ae4409"
-// 	hash := sha256.Sum256([]byte(payload))
-// 	payloadBytes := hash[:]
-// 	signatureBytes, _ := hex.DecodeString(signature)
-// 	publicKeyBytes, _ := hex.DecodeString(publicKey)
-// 	if secp256k1.VerifySignature(publicKeyBytes, payloadBytes, signatureBytes) {
-// 		println("Signature is valid")
-// 	} else {
-// 		println("Signature is invalid")
-// 	}
-// }
